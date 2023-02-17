ed79a5f10e6860e22c12d716477df9ece308fd45
pooya.oa <pooya.oladazimi@tib.eu>
2023-02-17 13:51:31 +0100

    implement the first version of sparql insert after create

--- ckanext/dcatapcrc/plugin.py
+++ ckanext/dcatapcrc/plugin.py
@@ -2,6 +2,12 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from flask import Blueprint
 from ckanext.dcatapcrc.controller import BaseController
+from ckanext.dcat.processors import RDFSerializer
+from xml.etree import ElementTree
+from SPARQLWrapper import SPARQLWrapper, POST
+
+
+SPARQL_ENDPOINT = "http://sparql11.test.service.tib.eu/fuseki/TestCKAN/update"
 
 
 class DcatapcrcPlugin(plugins.SingletonPlugin):
@@ -39,6 +45,58 @@ class DcatapcrcPlugin(plugins.SingletonPlugin):
     # IPackageController
 
     
+    def after_create(self, context, pkg_dict):
+        '''
+            Post the dataset metadata to the sparql endpoint
+        '''
+
+        serializer = RDFSerializer(profiles=pkg_dict.get('profiles'))
+        rdf_output = serializer.serialize_dataset(pkg_dict, _format="rdf")                                                
+        childNodeList = ElementTree.fromstring(rdf_output.decode('utf-8'))
+        for node in childNodeList:                     
+            print(node)
+
+
+        return pkg_dict
+
+    
+    def after_update(self, context, pkg_dict):
+        '''
+            Upadte an existing dataset metadata on the sparql endpoint
+        '''
+
+        # print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        # print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        # print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        # print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+        serializer = RDFSerializer(profiles=pkg_dict.get('profiles'))        
+        gr_dataset = serializer.graph_from_dataset(pkg_dict)                        
+        graph = serializer.g        
+        for s,p,o in graph:
+            # print(s,p,o)
+            if "http" in s:
+                s = "<" + s + ">"
+            if "http" in p:
+                p = "<" + p + ">"
+            if "http" in o:
+                o = "<" + o + ">"
+            if "http" not in o:
+                o = "'" + o + "'"
+            if s[0] == "N":
+                s = '_:' + s            
+            if o[0] == "N":
+                o = '_:' + o
+
+            
+            query = 'INSERT DATA{ ' + s + ' ' + p + ' ' + o + ' .  }'            
+            sparql = SPARQLWrapper(SPARQL_ENDPOINT)                        
+            sparql.setMethod(POST)
+            sparql.setQuery(query)
+            results = sparql.query()            
+        return pkg_dict
+    
+    
     def after_search(self, search_results, search_params):        
         return search_results
 
@@ -56,12 +114,6 @@ class DcatapcrcPlugin(plugins.SingletonPlugin):
 
     def delete(self, entity):
         return entity
-
-    def after_create(self, context, pkg_dict):
-        return pkg_dict
-
-    def after_update(self, context, pkg_dict):
-        return pkg_dict
 
     def after_show(self, context, pkg_dict):
         return pkg_dict

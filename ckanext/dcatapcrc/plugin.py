93698e86f7148e5dd50b2cecd87aa9be821ac661
pooya.oa <pooya.oladazimi@tib.eu>
2023-02-17 09:53:09 +0100

    implement IPackageController

--- ckanext/dcatapcrc/plugin.py
+++ ckanext/dcatapcrc/plugin.py
@@ -7,6 +7,7 @@ from ckanext.dcatapcrc.controller import BaseController
 class DcatapcrcPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.IPackageController)
 
     # IConfigurer
 
@@ -33,3 +34,43 @@ class DcatapcrcPlugin(plugins.SingletonPlugin):
             )   
         
         return blueprint 
+    
+
+    # IPackageController
+
+    
+    def after_search(self, search_results, search_params):        
+        return search_results
+
+    def after_delete(self, context, pkg_dict):
+        return pkg_dict
+    
+    def read(self, entity):
+        return entity
+
+    def create(self, entity):
+        return entity
+
+    def edit(self, entity):
+        return entity
+
+    def delete(self, entity):
+        return entity
+
+    def after_create(self, context, pkg_dict):
+        return pkg_dict
+
+    def after_update(self, context, pkg_dict):
+        return pkg_dict
+
+    def after_show(self, context, pkg_dict):
+        return pkg_dict
+
+    def before_search(self, search_params):
+        return search_params
+
+    def before_index(self, pkg_dict):
+        return pkg_dict
+
+    def before_view(self, pkg_dict):
+        return pkg_dict
